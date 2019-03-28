8eb48822c1e5b550adda5d9a8497d88cce254e2d
kreezxil <kreezxil@gmail.com>
2019-03-28 09:09:04 -0500

    1.11.2-1.3.4
    backported all of the new 1.12.2 features such as the config, allowing gravity blocks to be allowed, ability allow other blocks via config, and now build under allowance.

--- src/main/java/com/kreezcraft/nopoles/PoleHandler.java
+++ src/main/java/com/kreezcraft/nopoles/PoleHandler.java
@@ -1,6 +1,9 @@
 package com.kreezcraft.nopoles;
 
+import java.util.Arrays;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -14,15 +17,12 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber
 public class PoleHandler {
 
-	public static void msg(String msg) {
-		System.out.println(msg);
-	}
-
 	@SubscribeEvent
 	public static void NoColumns(PlaceEvent event) {
+
 		EntityPlayer player = event.getPlayer();
 
-		if (player.onGround || player.isInWater() || player.isOnLadder() || player.capabilities.allowFlying)
+		if (player.onGround || player.isOnLadder() || player.capabilities.allowFlying || player.isInWater())
 			return;
 
 		World world = event.getWorld();
@@ -30,13 +30,54 @@ public class PoleHandler {
 		double d0 = position.getX();
 		double d1 = position.getY();
 		double d2 = position.getZ();
-		Block block = event.getPlacedBlock().getBlock();
-		ItemStack stack = new ItemStack(Item.getItemFromBlock(block));
-		EntityItem haha = new EntityItem(world, d0, d1 ,d2, stack);
+//		Block block = event.getPlacedBlock().getBlock();
+		IBlockState blockstate = world.getBlockState(position);
 		
+		if(player.getPosition().getY()<position.getY()) return; //allow building under a block over the limit
+
+		if (Arrays.asList(PoleConfig.whitelist.blocks)
+				.contains(blockstate.getBlock().getRegistryName().toString() + ":" + blockstate.getBlock().getMetaFromState(blockstate))) {
+			// System.out.println("Block is safe");
+			return;
+		}
+
+
+		BlockPos check = position;
+		boolean destroy = true;
+		
+		for (int i = 0; i < PoleConfig.nerdpole.poleHeight; i++) {
+			//System.out.println("checking block #" + i);
+			if (!world.isAirBlock(check.south()) || !world.isAirBlock(check.north()) || !world.isAirBlock(check.west())
+					|| !world.isAirBlock(check.east())) {
+				// do nothing, not a nerdpole
+				destroy=false;
+				return;
+				
+			}
+			check = check.down();
+		}
+		
+//		System.out.println("column is a nerdpole? ["+destroy+"]");
+		
+		if(destroy) {
+			check = position;
+			while(world.isAirBlock(check.south()) && world.isAirBlock(check.north()) && world.isAirBlock(check.west())
+					&& world.isAirBlock(check.east())) {
+				System.out.println("Destroying pole at position "+check.toString());
+				ItemStack returnStack = new ItemStack(world.getBlockState(check).getBlock());
+				EntityItem damnPole = new EntityItem(world, check.getX(), check.getY(), check.getZ(), returnStack);
+				world.spawnEntity(damnPole);
+				world.setBlockToAir(check);
+				check = check.down();
+			}
+		}
+
+		ItemStack stack = new ItemStack(blockstate.getBlock());
+		EntityItem haha = new EntityItem(world, d0, d1, d2, stack);
+
 		world.spawnEntity(haha);
-		
-		event.getWorld().setBlockToAir(event.getPos());
+
+		world.setBlockToAir(event.getPos());
 
 	}
 
